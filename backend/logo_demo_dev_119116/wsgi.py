4080c738b4b5e3305ef5cb5c0f9ffcaf6d8c31db
Crowdbotics <engineering@crowdbotics.com>
2023-09-29 16:06:48 +0000

    Initial commit

---
+++ backend/logo_demo_dev_119116/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for logo_demo_dev_119116 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "logo_demo_dev_119116.settings"
+)
+
+application = get_wsgi_application()
